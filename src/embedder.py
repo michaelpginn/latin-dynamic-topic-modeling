3266677eaa8c35c1a248c14320cc663672fe1925
michaelpginn <milespergallon271@gmail.com>
2023-04-19 18:13:24 -0600

    Fix embedding code

--- src/embedder.py
+++ src/embedder.py
@@ -3,14 +3,14 @@ from tqdm import tqdm
 import numpy as np
 from cltk import NLP
 from bertopic.backend import BaseEmbedder
-from corpus import load_corpus
 import multiprocessing as mp
+import math
+from corpus import load_corpus
 
 os.environ["CLTK_DATA"] = "../data"
 
-def compute_document_embeddings(docs, bar_queue) -> dict:
-    """Creates a dict of document embeddings, using the average sentence embedding.
-    :returns: A dict where the key is the document text and the value is the embedding vector
+def compute_document_embeddings(docs, bar_queue) -> list:
+    """Creates a list of document embeddings, using the average sentence embedding.
     """
 
     # Create the NLP pipeline for Latin
@@ -30,12 +30,12 @@ def compute_document_embeddings(docs, bar_queue) -> dict:
    
     # Compute embeddings for every document
     # print("Computing document embeddings...")
-    all_doc_embeddings = dict()
+    all_doc_embeddings = []
     for index, text in enumerate(docs):
         # print(f"Task {task_num}: {index}/{len(docs)}")
             
         doc = nlp.analyze(text[1])
-        all_doc_embeddings[text[1]] = average_sentence_embeddings(doc)
+        all_doc_embeddings.append(average_sentence_embeddings(doc))
         bar_queue.put_nowait(1)
 
 
@@ -54,7 +54,7 @@ class LatinEmbedder(BaseEmbedder):
 
 
 # Helper funcs for multithreading
-def _compute_embeddings_chunk(index_range, documents, all_embeddings, bar_queue):
+def _compute_embeddings_chunk(index_range, position, documents, all_embeddings, bar_queue):
     embeddings = compute_document_embeddings(documents[index_range.start:index_range.stop], bar_queue) 
     all_embeddings[position] = embeddings
 
@@ -67,22 +67,22 @@ def _update_bar(q, total_documents):
     
 
 def main():
-    documents = load_corpus()
+    documents = load_corpus()[:500]
     
     # Create embeddings in parallel
     bar_queue = mp.Queue()
-    all_embeddings = dict()
+    all_embeddings = mp.Manager().dict()
     
     num_threads = 8
-    chunk_size = int(len(documents) / num_threads)
-    processes = [ 
-        mp.Process(target=_compute_embeddings_chunk, 
-                   args=(range(chunk_size * i, chunk_size * (i+1)),
-                         documents,
-                         all_embeddings, 
-                         bar_queue)
-                  ) for i in range(num_threads) 
-    ]
+    chunk_size = math.ceil(len(documents) / num_threads)
+    processes = []
+    for i in range(num_threads):
+        index_range = range(chunk_size * i, chunk_size * (i+1))
+        print(index_range)
+        process = mp.Process(target=_compute_embeddings_chunk,
+                             args=(index_range, i, documents, all_embeddings, bar_queue))
+        processes.append(process)
+
     bar_process = mp.Process(target=_update_bar, args=(bar_queue,len(documents)), daemon=True)
     bar_process.start()
     
@@ -91,11 +91,13 @@ def main():
         
     for p in processes:
         p.join()
-        
+
+    bar_process.kill()
+
     embeddings_list = []
     for i in range(num_threads):
-        embeddings_list.append(all_embeddings[i])
-    np.save('doc_embeddings.npy', np.array(embeddings_list.values(), dtype=object), allow_pickle=True)
+        embeddings_list += all_embeddings[i]
+    np.save('doc_embeddings.npy', np.array(embeddings_list, dtype=object), allow_pickle=True)
 
     
 if __name__ == "__main__":
